8e742ccab4f454b06f76d044924221bf9354352f
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2025-01-13 04:15:26 +0700

    Menambahkan operasi CRUD dan pengambilan data Mahasiswa

--- app/src/main/java/com/example/firebase/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/firebase/repository/MahasiswaRepository.kt
@@ -1,4 +1,17 @@
 package com.example.firebase.repository
 
-interface MahasiswaRepository {
+import com.example.firebase.model.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
+interface MahasiswaRepository{
+
+    suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
+
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    suspend fun getMahasiswaById(nim: String): Flow<Mahasiswa>
 }
