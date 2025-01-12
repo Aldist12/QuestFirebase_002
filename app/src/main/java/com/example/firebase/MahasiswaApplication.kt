abcfe4e582e2927da727ff7dda6879c87a809e84
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2025-01-13 04:24:07 +0700

    Menambahkan kelas MahasiswaApplication untuk inisialisasi dependency injection menggunakan AppContainer.

--- app/src/main/java/com/example/firebase/MahasiswaApplication.kt
+++ app/src/main/java/com/example/firebase/MahasiswaApplication.kt
@@ -1,2 +1,13 @@
 package com.example.firebase
 
+import android.app.Application
+import com.example.firebase.depedenciesinjection.AppContainer
+import com.example.firebase.depedenciesinjection.MahasiswaContainer
+
+class MahasiswaApplication: Application() {
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = MahasiswaContainer()
+    }
+}
